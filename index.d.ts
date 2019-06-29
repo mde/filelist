27bea4f7a1fdf243ff63484b2a074fbfa3767de8
FuriouZz <christophe.massolin@gmail.com>
2019-06-29 21:02:19 +0200

    Update typing

--- index.d.ts
+++ index.d.ts
@@ -14,6 +14,7 @@ declare module "filelist" {
     }
     items: string[]
     static clone() : FileList
+    static debug : boolean
     toArray() : string[]
     include(options:any, ...items:string[]) : void
     exclude(...items:string[]) : void
